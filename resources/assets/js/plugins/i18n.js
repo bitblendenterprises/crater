a4d6f795470367f058e89d69d3fc315e17d2f91f
Ryan Lupeșcu <bitblendtechnologies@gmail.com>
2020-01-19 17:57:04 +0100

    NL translations

--- resources/assets/js/plugins/i18n.js
+++ resources/assets/js/plugins/i18n.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import en from './en.json'
+import en from './nl.json'
 import fr from './fr.json'
 import es from './es.json'
 import ar from './ar.json'
@@ -10,15 +11,16 @@ import pt_BR from './pt-br.json'
 Vue.use(VueI18n)
 
 const i18n = new VueI18n({
-  locale: 'en',
-  messages: {
-    en,
-    fr,
-    es,
-    ar,
-    de,
-    pt_BR
-  }
+    locale: 'en',
+    messages: {
+        en,
+        nl,
+        fr,
+        es,
+        ar,
+        de,
+        pt_BR
+    }
 })
 
 export default i18n
